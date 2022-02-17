1b771f8a9117ff600165ccfd0d9b6695e5509dac
Sid Menon <menon.sid.k@gmail.com>
2022-02-17 15:02:36 -0500

    [rich] remove pretty-printing because not available in repl.it

--- check32.py
+++ check32.py
@@ -1,10 +1,5 @@
 """CS 32 helper library for Act I, Scene II"""
 
-from rich.traceback import install
-
-install(show_locals=True)
-
-
 def check_arg(arg: object, ttype: type, arg_name: str):
     if not isinstance(arg, ttype):
         raise TypeError(
